f6598cb24fd79222a71dc3dd4782dc20a7129fdf
severin12am <140651547+severin12am@users.noreply.github.com>
2025-03-12 19:13:54 +0300

    Update supabase.ts

--- project/src/lib/supabase.ts
+++ project/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://uwursdathnpwsiztdxzu.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3dXJzZGF0aG5wd3NpenRkeHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MjU5NzUsImV4cCI6MjA1NzMwMTk3NX0.P-LZ51W23JX1h3L1bOQ3inpJ68MtxogRdCB18aEBlfg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
